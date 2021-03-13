8177ad62cdd540d403913d2c66a686ee599d420d
nvtamnt99 <nvtamnt99>
2021-03-14 02:06:15 +0700

    Code New User Function

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -5,15 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shopme.common.entity.PhanQuyen;
 import com.shopme.common.entity.TaiKhoan;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepo;
+	
+	@Autowired
+	private RoleRepository roleRepo;
 	
 	public List<TaiKhoan> listAll(){
-		return (List<TaiKhoan>) repo.findAll();
+		return (List<TaiKhoan>) userRepo.findAll();
+	}
+	
+	public List<PhanQuyen> listRoles(){
+		return (List<PhanQuyen>) roleRepo.findAll();
+	}
+
+	public void save(TaiKhoan user) {
+		userRepo.save(user);	
 	}
 }
