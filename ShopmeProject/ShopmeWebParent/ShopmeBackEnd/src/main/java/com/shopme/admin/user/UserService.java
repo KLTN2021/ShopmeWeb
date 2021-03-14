8903a3c3fdb0c2a48d1800b713d8ff5eab28363a
nvtamnt99 <nvtamnt99>
2021-03-14 10:42:32 +0700

    Encode User Password

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.shopme.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.PhanQuyen;
@@ -17,6 +18,9 @@ public class UserService {
 	@Autowired
 	private RoleRepository roleRepo;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public List<TaiKhoan> listAll(){
 		return (List<TaiKhoan>) userRepo.findAll();
 	}
@@ -26,6 +30,12 @@ public class UserService {
 	}
 
 	public void save(TaiKhoan user) {
+		encodePassword(user);
 		userRepo.save(user);	
+	}
+	
+	private void encodePassword(TaiKhoan user) {
+		String encodedPassword = passwordEncoder.encode(user.getMatKhau());
+		user.setMatKhau(encodedPassword);
 	}
 }
