daaef266ca4b05c63341bc35bbe465fc9afd278f
nvtamnt99 <nvtamnt99>
2021-03-24 00:02:55 +0700

    Code Pagination for Users List

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -3,11 +3,12 @@ package com.shopme.admin.user;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.shopme.common.entity.TaiKhoan;
 
-public interface UserRepository extends CrudRepository<TaiKhoan, Integer>{
+public interface UserRepository extends PagingAndSortingRepository<TaiKhoan, Integer>{
 	@Query("SELECT u FROM TaiKhoan u WHERE u.email = :email")
 	public TaiKhoan getUserByEmail(@Param("email") String email);
 	
