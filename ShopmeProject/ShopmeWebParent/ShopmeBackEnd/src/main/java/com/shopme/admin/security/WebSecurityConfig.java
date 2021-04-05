c99bc6350d5388e2fff3ed45ddec86634cd80db1
nvtamnt99 <nvtamnt99>
2021-04-05 23:01:34 +0700

    Code User Authorization Basics

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -42,17 +42,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.anyRequest().authenticated()
-		.and()
-		.formLogin()
-			.loginPage("/dangnhap")
-			.usernameParameter("email")
-			.permitAll()
-		 .and().logout().permitAll()
-		 	.and()
-		 		.rememberMe()
-		 			.key("AbcDefgHijKlmnOpqrs_1234567890")
-		 			.tokenValiditySeconds(7 * 24 * 60 * 60);
+			.antMatchers("/nguoidung/**").hasAuthority("Quản trị viên")
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/dangnhap")
+				.usernameParameter("email")
+				.permitAll()
+			 .and().logout().permitAll()
+			 	.and()
+			 		.rememberMe()
+			 			.key("AbcDefgHijKlmnOpqrs_1234567890")
+			 			.tokenValiditySeconds(7 * 24 * 60 * 60);
 					
 	}
 
