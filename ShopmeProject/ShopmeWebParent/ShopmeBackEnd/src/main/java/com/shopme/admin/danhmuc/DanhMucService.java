a4e8265446f1231486ee0e20790c15eb6a59cf9f
nvtamnt99 <nvtamnt99>
2021-04-14 11:35:05 +0700

    refactor code

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/danhmuc/DanhMucService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/danhmuc/DanhMucService.java
@@ -23,7 +23,7 @@ import com.shopme.common.entity.DanhMuc;
 @Service
 @Transactional
 public class DanhMucService {
-	public static final int ROOT_CATEGORIES_PER_PAGE = 3;
+	public static final int ROOT_CATEGORIES_PER_PAGE = 4;
 	
 	@Autowired
 	private DanhMucRepository repo;
